7246030a9df9e85e7bff68b6d08afbca6757b700
frozenreflex <frozenreflex1@gmail.com>
2022-07-15 16:13:10 -0500

    more mesh renderer

--- MeshRendererConnector.cs
+++ MeshRendererConnector.cs
@@ -8,35 +8,55 @@ namespace Thundaga
 {
     public class MeshRendererConnectorPacket : ConnectorPacket<MeshRendererConnector>
     {
-        //todo: make meshrenderer and skinnedmeshrenderer packets use generics to avoid duplicating code
-        //will require generic patching shenanigans
         private bool _isAssetAvailable;
+        private bool _meshWasChanged;
+        private FrooxEngine.Mesh _mesh;
 
         public MeshRendererConnectorPacket(MeshRendererConnector connector)
         {
             _connector = connector;
-            _isAssetAvailable = connector.Owner.Mesh.IsAssetAvailable;
+            var owner = connector.Owner;
+            var mesh = owner.Mesh;
+            _isAssetAvailable = mesh.IsAssetAvailable;
+            _meshWasChanged = mesh.GetWasChangedAndClear();
+            _mesh = mesh.Asset;
         }
 
         protected virtual void OnAttachRenderer()
+        {
+        }
+        
+        protected virtual void ChildApplyChange()
         {
         }
 
         protected virtual void OnCleanupRenderer()
         {
         }
-        
+
+        protected virtual void AssignMesh(Renderer renderer, Mesh mesh)
+        {
+        }
         public override void ApplyChange()
         {
             if (_isAssetAvailable)
             {
+                var filter = MeshRendererConnectorPatches.get_UseMeshFilter(_connector);
+                ChildApplyChange();
                 if (_connector.MeshRenderer == null)
                 {
                     var gameObject = MeshRendererConnectorPatches.get_attachedGameObject(_connector);
-                    if (MeshRendererConnectorPatches.get_UseMeshFilter(_connector))
-                        MeshRendererConnectorInfo.MeshFilter.SetValue(_connector, gameObject.AddComponent<MeshFilter>());
+                    if (filter)
+                        MeshRendererConnectorInfo.MeshFilter.SetValue(_connector,
+                            gameObject.AddComponent<MeshFilter>());
                     MeshRendererConnectorPatches.set_MeshRenderer(_connector, gameObject.AddComponent<MeshRenderer>());
                     OnAttachRenderer();
+                }
+                if (_meshWasChanged)
+                {
+                    var unity = _mesh.GetUnity();
+                    if (filter) ((MeshFilter)MeshRendererConnectorInfo.MeshFilter.GetValue(_connector)).sharedMesh = unity;
+                    else AssignMesh(_connector.MeshRenderer, unity);
                 }
             }
             else
